4be6c99d61f58d2882da6bc994476d9d8241e24f
bigiotec01 <118586427+bigiotec01@users.noreply.github.com>
2026-02-26 23:28:18 -0400

    Fix merge conflicts

--- firebase-config.js
+++ firebase-config.js
@@ -5,21 +5,12 @@
 // ========================================
 
 const firebaseConfig = {
-<<<<<<< HEAD
   apiKey: "AIzaSyCfBTHp9PMjHBqjOcCigGaZlA7c-nEXe-o",
   authDomain: "creacciones-ivelisse.firebaseapp.com",
   projectId: "creacciones-ivelisse",
   storageBucket: "creacciones-ivelisse.firebasestorage.app",
   messagingSenderId: "406396161888",
   appId: "1:406396161888:web:2521645340e2805b965805"
-=======
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO_ID",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "TU_MESSAGING_ID",
-  appId: "TU_APP_ID"
->>>>>>> 064e8e93553bf1f8df49181c6bf4ea729f7c7835
 };
 
 // Inicializar Firebase
